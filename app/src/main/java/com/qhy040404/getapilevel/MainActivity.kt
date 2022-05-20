db22b9f8fd5b8d669f615e33d3bcea55a8c77bea
qhy040404 <qhy040404@163.com>
2022-05-20 16:55:03 +0800

    Initial commit

---
+++ app/src/main/java/com/qhy040404/getapilevel/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.qhy040404.getapilevel
+
+import android.os.Bundle
+import android.os.Looper
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        Thread(get()).start()
+    }
+
+    private inner class get:Runnable {
+        override fun run() {
+            Looper.prepare()
+
+            val textView:TextView = findViewById(R.id.ApiTextView)
+            textView.text = android.os.Build.VERSION.SDK_INT.toString()
+        }
+    }
+}
